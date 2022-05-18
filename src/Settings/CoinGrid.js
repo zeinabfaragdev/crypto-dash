144e4f4b710bd6412c95bfc99f9e1b402b1d4db9
Zeinab Farag <zeinabfarag454@gmail.com>
2022-05-18 18:08:49 -0400

    grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -4,7 +4,7 @@ import CoinTile from "./CoinTile";
 
 const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
   grid-gap: 15px;
   margin-top: 40px;
 `;
