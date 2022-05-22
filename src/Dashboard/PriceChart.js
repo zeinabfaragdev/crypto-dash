33261dc01ee8832a1f234b034f3080bfb537e678
Zeinab Farag <zeinabfarag454@gmail.com>
2022-05-22 19:51:32 -0400

    highcharts theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,12 +1,15 @@
-import HighChartsConfig from "./HighChartsConfig";
+import HighchartsConfig from "./HighchartsConfig";
 import { Tile } from "../Shared/Tile";
 import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
+import HighchartsTheme from "./HighChartsTheme";
+
+Highcharts.setOptions(HighchartsTheme);
 
 const PriceChart = () => {
   return (
     <Tile>
-      <HighchartsReact highcharts={Highcharts} options={HighChartsConfig()} />
+      <HighchartsReact highcharts={Highcharts} options={HighchartsConfig()} />
     </Tile>
   );
 };
